fe332fcaaf66db986d7636b170a969ee99145266
caiodominguesbandtec <caio.domingues@bandtec.com.br>
2020-09-04 15:43:09 -0300

    criação de métodos dentro do controller

--- avaliacao-continuada/src/main/java/br/com/bandtec/avaliacaocontinuada/ProgramaController.java
+++ avaliacao-continuada/src/main/java/br/com/bandtec/avaliacaocontinuada/ProgramaController.java
@@ -1,6 +1,7 @@
 package br.com.bandtec.avaliacaocontinuada;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,24 +14,64 @@ public class ProgramaController {
     private List<Programa> programas = new ArrayList<>();
 
     @GetMapping
-    public List listarProgramas() {
-        return programas;
+    public ResponseEntity listarProgramas() {
+        if (programas.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        else {
+            return ResponseEntity.ok(programas);
+        }
     }
 
     @GetMapping("/{id}")
-    public Programa consultarPrograma (@PathVariable int id) {
-        return programas.get(id-1);
+    public ResponseEntity consultarPrograma (@PathVariable int id) {
+        if (programas.size() < id){
+            return ResponseEntity.notFound().build();
+        }
+        else{
+            return ResponseEntity.ok(programas.get(id-1));
+        }
     }
 
     @PostMapping
-    public String cadastraPrograma(@RequestBody Programa novoPrograma) {
+    public ResponseEntity cadastraPrograma(@RequestBody Programa novoPrograma) {
         programas.add(novoPrograma);
-        return "Programa cadastrado com sucesso";
+        return ResponseEntity.created(null).build();
     }
 
     @DeleteMapping("/{id}")
-    public String deletarPrograma(@PathVariable int id){
-        programas.remove(id-1);
-        return "Programa número " + id + "excluído com sucesso";
+    public ResponseEntity deletarPrograma(@PathVariable int id){
+        if (programas.size() < id) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            programas.remove(id - 1);
+            return ResponseEntity.ok().build();
+        }
     }
+
+    @GetMapping("/lucros")
+    public ResponseEntity lucrosGeral(@PathVariable int id){
+        if (programas.size() < id) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            Double total = 0.0;
+            for (Programa p : programas) {
+                total += p.calculaLucro();
+            }
+            return ResponseEntity.ok("O lucro total dessa emissora foi de " + total);
+        }
+    }
+
+/*    @GetMapping("/lucros/{id}")
+    public ResponseEntity lucrosPrograma(@PathVariable int id){
+        if (programas.size() < id) {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok("O lucro total dessa emissora foi de " + programas);
+        }
+    }
+*/
 }
